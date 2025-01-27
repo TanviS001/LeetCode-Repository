49ec05a6d45057268a0aec18c9326e300fe46ccc
Tanvi <145559687+TanviS001@users.noreply.github.com>
2025-01-27 13:54:12 +0530

    Update SmallerNumbersThanCurrent.py

--- SmallerNumbersThanCurrent.py
+++ SmallerNumbersThanCurrent.py
@@ -1,5 +1,5 @@
 #1365. How Many Numbers Are Smaller Than the Current Number
-
+# Approach 1:
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         length = len(nums)
@@ -11,3 +11,5 @@ class Solution:
                     count=count+1
             ret.append(count)
         return ret 
+        
+# Learned Approach:
