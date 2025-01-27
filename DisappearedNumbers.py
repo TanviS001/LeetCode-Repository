2919b17a334e1edaa4e9c2786ae02940410f2bc7
Tanvi <145559687+TanviS001@users.noreply.github.com>
2025-01-27 12:03:47 +0530

    Update DisappearedNumbers.py

--- DisappearedNumbers.py
+++ DisappearedNumbers.py
@@ -1,10 +1,12 @@
 # 448. Find All Numbers Disappeared in an Array
 
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        set_nums=set(nums)
         length=len(nums)
         ans=[]
         for i in range(1, length+1):
-            if i not in nums:
+            if i not in set_nums:
                 ans.append(i)
         return ans
