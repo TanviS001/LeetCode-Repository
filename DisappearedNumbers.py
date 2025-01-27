ac72a35fb8d59345dcf6fa86127391a38512332d
Tanvi <145559687+TanviS001@users.noreply.github.com>
2025-01-27 11:52:16 +0530

    Update DisappearedNumbers.py

--- DisappearedNumbers.py
+++ DisappearedNumbers.py
@@ -2,10 +2,9 @@
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        sorted_arr=sorted(nums)
         length=len(nums)
         ans=[]
         for i in range(1, length+1):
-            if i not in sorted_arr:
+            if i not in nums:
                 ans.append(i)
         return ans
