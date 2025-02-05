c261979b2ab820c3adc08c9baf8ce5673ddb80a6
Tanvi <145559687+TanviS001@users.noreply.github.com>
2025-02-05 19:13:15 +0530

    Create PalindromeNumber.py

---
+++ PalindromeNumber.py
@@ -0,0 +1,12 @@
+# 9. Palindrome Number
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        num=str(x)
+        rev=num[::-1]
+
+        if num==rev:
+            return True
+        else:
+            return False
